a795795b435e1ccbeca3c810ff25599c2f29f584
Semyon Fedoseev <samgxlden@gmail.com>
2022-08-19 23:28:21 +0300

    changed files tree for yandex tests

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(morgan("dev"))
-app.use(express.static("../frontend/dist"))
+app.use(express.static("./dist"))
 
 app.listen(port, () => {
   console.log("Server started on http://localhost:" + port)
